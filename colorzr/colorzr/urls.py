c025018a07577428b0ec7051c3f48682d007cd21
Eric Stavarache <ericptst@gmail.com>
2018-05-02 16:17:47 +0300

    Make coconuts the default page

--- colorzr/colorzr/urls.py
+++ colorzr/colorzr/urls.py
@@ -36,10 +36,10 @@ api_urlpatterns = [
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('accounts/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('coconuts.urls')),
 
+    path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
-    path('coconut/', include('coconuts.urls')),
+    path('accounts/', include('rest_framework.urls', namespace='rest_framework')),
+    path('routes/', include(router.urls)),
 ]
