7969a6957d797b2127d70717281281689476aa3c
Lucian Bicsi <lucianbicsi@gmail.com>
2018-05-09 11:12:26 +0300

    Added one extra test case

--- colorzr/accounts/tests.py
+++ colorzr/accounts/tests.py
@@ -1,21 +1,43 @@
+from django.contrib import auth
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
-from django.urls import reverse, resolve
-from django.contrib import auth
+from django.urls import reverse
+from friendship.models import Follow
 
 from .models import Profile
+
+
+class DatabaseTestCase(TestCase):
+    def setUp(self):
+        self.adam = User.objects.create_user('Adam', 'admin@test.com', 'password123')
+        self.bob = User.objects.create_user('Bob', 'bob@yahoo.com', 'password123')
+        self.charlie = User.objects.create_user('Charlie', 'charlie@yahoo.com', 'password123')
+
+        Follow.objects.add_follower(self.adam, self.bob)
+        Follow.objects.add_follower(self.adam, self.charlie)
+        Follow.objects.add_follower(self.bob, self.charlie)
+
+        self.client = Client()
+
+    def test_profile_created(self):
+        self.assertTrue(Profile.objects.filter(user__username='Adam').exists(),
+                        'Profile should be created for each user')
+        self.assertFalse(Profile.objects.filter(user__username='does_not_exist').exists(),
+                         'User not added, so profile should not exist')
+
+    def test_follower_count(self):
+        self.assertEqual(self.adam.profile.follower_count(), 0)
+        self.assertEqual(self.bob.profile.follower_count(), 1)
+        self.assertEqual(self.charlie.profile.follower_count(), 2)
+
+        self.assertIn(self.adam, Follow.objects.followers(self.charlie))
+        self.assertIn(self.charlie, Follow.objects.following(self.bob))
 
 
 class LoggedOutTestCase(TestCase):
     def setUp(self):
         User.objects.create_user('admin', 'admin@test.com', 'password123')
         self.client = Client()
-
-    def test_profile_created(self):
-        self.assertTrue(Profile.objects.filter(user__username='admin').exists(),
-                        'Profile should be created for each user')
-        self.assertFalse(Profile.objects.filter(user__username='does_not_exist').exists(),
-                         'User not added, so profile should not exist')
 
     def test_account_login_logout(self):
         self.client.post(reverse('login'), {'username': 'admin', 'password': 'password123'})
@@ -35,7 +57,6 @@ class LoggedInTestCase(TestCase):
     def test_homepage_redirect(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 302)
-
 
 
 
